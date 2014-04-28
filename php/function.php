688128e84e590ba5d98cec7cb2290b1a051af02f
koschi1988 <kaikosche@gmail.com>
2014-04-28 11:57:36 +0200

    https://github.com/usersrc/portal5/issues/3 Datenbank mit angeschlossen und nun ist es möglich, seine Abwesenheit einzutragen. Es fehlt noch, dass ausgelesen wird, welcher User gerade "angemeldet" ist.
    Signed-off-by: koschi1988 <kaikosche@gmail.com>

--- php/function.php
+++ php/function.php
@@ -1,6 +1,13 @@
 <?php
+/*Für die Formatierung von MySQL (yyyy-mm-dd) zur deutschen Datumanzeige (dd.mm.yyyy)*/
 function date_mysql2german($date) {
 		$d    =    explode("-",$date);
 		return    sprintf("%02d.%02d.%04d", $d[2], $d[1], $d[0]);
+};
+
+/*Für die Formatierung von der deutschen Datumsanzeige (dd.mm.yyyy) in die MySQL-Schreibweise (yyyy-mm-dd)*/
+function date_german2mysql($date) {
+		$d    =    explode(".",$date);
+		return    sprintf("%04d-%02d-%02d", $d[2], $d[1], $d[0]);
 };
 ?>
